c63b92a97feaa42e7ccebe42c3ee2fb745b2861d
keval-digicorp <keval.patel@digi-corp.com>
2026-08-19 12:49:55 +0530

    test ci/cd

--- todo-ec2-app/frontend/src/App.jsx
+++ todo-ec2-app/frontend/src/App.jsx
@@ -53,7 +53,7 @@ export default function App() {
             style={styles.input}
             value={text}
             onChange={(e) => setText(e.target.value)}
-            placeholder="What needs doing?"
+            placeholder="What need to do?"
           />
           <button style={styles.addBtn} type="submit">Add</button>
         </form>
