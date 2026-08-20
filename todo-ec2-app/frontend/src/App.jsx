ff6ef1287b3324097ce53a259beb3747a8db8a48
keval-digicorp <keval.patel@digi-corp.com>
2026-08-20 14:49:50 +0530

    Remove GitHub Actions; use CodePipeline auto trigger

--- todo-ec2-app/frontend/src/App.jsx
+++ todo-ec2-app/frontend/src/App.jsx
@@ -53,7 +53,7 @@ export default function App() {
             style={styles.input}
             value={text}
             onChange={(e) => setText(e.target.value)}
-            placeholder="What need to do?"
+            placeholder="What needs to doing?"
           />
           <button style={styles.addBtn} type="submit">Add</button>
         </form>
